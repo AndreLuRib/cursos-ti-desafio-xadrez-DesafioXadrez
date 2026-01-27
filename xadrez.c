0ccaf64e9bd18a3d3eeab7156d8e21c449bb7ace
AndreLuRib <andre0605ribeiro@gmail.com>
2026-01-27 23:57:09 +0000

    Nivel aventureiro: Movendo o cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-   //t = torre, b = bispo, r = rainha
-    int t = 0, b = 0, r = 0;
+   //t = torre, b = bispo, r = rainha.
+    int t = 0, b = 0, r = 0, i, passos;
 
     printf("Bem-vindo ao Xadrez!\n");
     
-    // Mover a torre 5 casas para a direita
+    // Mover a torre 5 casas para a direita.
     
     printf("Movendo a torre 5 casas para a direita...\n");
 
@@ -14,7 +14,7 @@ int main(){
         printf("Direita\n");
     }
 
-    // Mover o bispo 5 casas para cima e a direita
+    // Mover o bispo 5 casas para cima e a direita.
 
     printf("Movendo o bispo 5 casas para cima e a direita...\n");
 
@@ -23,7 +23,7 @@ int main(){
         b++;
     }
 
-    // Mover a rainha 8 casas para a esquerda
+    // Mover a rainha 8 casas para a esquerda.
 
     printf("Movendo a rainha 8 casas para a esquerda...\n");
 
@@ -31,7 +31,31 @@ int main(){
         printf("Esquerda\n");
         r++;
     } while( r < 8);
- 
+
+    // Mover o cavalo 2 casas paara baixo e uma a esquerda.
+
+    printf("Movendo o cavalo 2 casas para baixo e uma a esquerda...\n");
+
+    for ( i = 0; i < 2; i++) {
+
+        if (i == 0) {
+            // primeiro movimento: 2 casas para baixo.
+            passos = 0;
+
+            while ( passos < 2) {
+                printf("Baixo\n");
+                passos++;
+            }
+        } else {
+            // Segundo movimento: 1 casa para a esquerda.
+            passos = 0;
+
+            while (passos < 1){
+                printf("Esquerda\n");
+                passos++;
+            }
+        }
+    }
 
 
     return 0;
